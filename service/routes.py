4491089a8fe4c7d1f91dea99a8b143a1473583b8
wy33 <yangw031@gmail.com>
2025-05-04 16:09:11 -0400

    list accounts

--- service/routes.py
+++ service/routes.py
@@ -61,8 +61,19 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
 
-# ... place you code here to LIST accounts ...
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
@@ -78,7 +89,7 @@ def get_accounts(account_id):
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
 
     return account.serialize(), status.HTTP_200_OK
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -126,12 +126,20 @@ class TestAccountService(TestCase):
     def test_read_an_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
-        response = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
+        response = self.client.get(f"{BASE_URL}/{account.id}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
     
     def test_account_not_found(self):
         """It should not Read an Account that is not found"""
-        response = self.client.get(f"{BASE_URL}/0", content_type="application/json")
+        response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_list_accounts(self):
+        """It should List all Accounts"""
+        self._create_accounts(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
